45e88fcbdae9491b10b1ed2c340f0a048d917b70
Aryanchoudhary12 <145906449+Aryanchoudhary12@users.noreply.github.com>
2025-08-05 15:22:58 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,9 @@ app.use("/", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-  })
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 app.listen(Port, () => {
   console.log(`server is running on http://localhost:${Port}`);
