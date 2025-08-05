e58a2b84e5a3796940fb78f4184a07488245b906
Aryanchoudhary12 <145906449+Aryanchoudhary12@users.noreply.github.com>
2025-08-05 14:15:58 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frondend","dist","index.html"));
+    res.sendFile(path.join(__dirname,"../frontdend","dist","index.html"));
   })
 }
 app.listen(Port, () => {
